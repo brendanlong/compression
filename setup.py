dd198d7141c48887be0e63c95e537086f47e27f0
Brendan Long <self@brendanlong.com>
2015-04-12 14:54:41 -0500

    Add Tox support and fix Python version on CircleCI.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='Compression',
+    version='1.0',
+    description='A personal compression project.',
+    author='Brendan Long',
+    author_email='self@brendanlong.com',
+    url='https://github.com/brendanlong/compression',
+    packages=['compression'],
+)
